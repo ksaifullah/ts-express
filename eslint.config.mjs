4c0219164a2edc6f4ab94b9bfda92e4445749dd0
Khalid Saifullah <khalid@outlook.com.au>
2025-07-18 10:43:00 +1000

    feat: typescript boilerplate

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    ignores: [
+      'dist/*',
+      'coverage/*',
+      '**/*.d.ts',
+      '/src/public/',
+      '/src/types/',
+    ],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    ignores: [
+      'dist/*',
+      'coverage/*',
+      '**/*.d.ts',
+      '/src/public/',
+      '/src/types/',
+    ],
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'max-len': ['error', { code: 80, ignoreUrls: true }],
+      'linebreak-style': ['error', 'unix'],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'block-spacing': ['error', 'always'],
+      'space-in-parens': ['error', 'never'],
+    },
+  },
+  tseslint.configs.recommended,
+]);
