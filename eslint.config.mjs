68bc47794a9017f440f01cc032dfb064d930048f
Khalid Saifullah <khalid@outlook.com.au>
2025-07-27 20:32:28 +1000

    chore: update jest and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,24 @@
-import js from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import { defineConfig } from 'eslint/config';
+// @ts-check
+
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig([
+export default tseslint.config(
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     ignores: [
       'dist/*',
       'coverage/*',
       '**/*.d.ts',
-      '/src/public/',
-      '/src/types/',
+      'src/public/*',
     ],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    ignores: [
-      'dist/*',
-      'coverage/*',
-      '**/*.d.ts',
-      '/src/public/',
-      '/src/types/',
-    ],
-    plugins: { '@stylistic': stylistic },
     rules: {
       'indent': ['error', 2],
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'max-len': ['error', { code: 80, ignoreUrls: true }],
-      'linebreak-style': ['error', 'unix'],
-      'keyword-spacing': ['error', { before: true, after: true }],
-      'block-spacing': ['error', 'always'],
-      'space-in-parens': ['error', 'never'],
     },
   },
-  tseslint.configs.recommended,
-]);
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+);
--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'jest';
+import { createDefaultPreset } from 'ts-jest';
 
 const config: Config = {
+  ...createDefaultPreset(),
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
